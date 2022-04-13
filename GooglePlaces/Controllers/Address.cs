a013d108f3f45adb753249046f4ee8b595a41dc3
maltechfreelancer <maltechfreelancer01@gmail.com>
2022-04-13 22:37:29 +0530

    Add project files.

---
+++ GooglePlaces/Controllers/Address.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GooglePlaces.Controllers
+{
+    public class Address : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
